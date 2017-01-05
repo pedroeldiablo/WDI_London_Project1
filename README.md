cd66eb972692ddff65c9b584e7cc17368e472c38
Peter Williams <pedroeldiablo@yahoo.com>
2017-01-05 18:52:29 +0000

    resolved some indentation issues and working on readme

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-# WDI_London_Project1
-A JS Game
+# SeeingRed
+## A JS Game
+
+### Play the game [here]:https://seeing-red.herokuapp.com/
+
+##Overview
 
 The idea for Seeing Red started with the idea of developing a vision based game.
 
-On looking into possible options for the color scheme I encountered research into color blindness, color vision deficiency, or CVD and how to make a more accessible web.
+On looking into possible options for the color scheme I encountered research into color blindness, color vision deficiency or CVD and how to make a more accessible web.
 
-The web is a very visual environment, but for a significant portion of the population (around 6% of men and 1 in 200 women) a significant amount of this information is hard to distinguish.
+The web is a very visual environment, but for a significant portion of the population ([around 8% of men and 1 in 200 women]: http://www.colourblindawareness.org/colour-blindness/) a significant amount of this information is hard to distinguish.
 
 At this time I decided to look into the possibility of turning this into a more practical application.
 
@@ -16,8 +20,8 @@ https://en.wikipedia.org/wiki/Ishihara_test
 
 In the standard version of the game the color palette has been selected to be tonal if viewed in grey scale. This means that for the majority of users they will be able to play regardless of any CVD.
 
-The game play is fairly simple. Once created the board disappears in two ways. Firstly the bottom dots in the grid are eaten. Secondly at timed intervals a whole color disappears.
+The game play is fairly simple. Once created the board disappears in two ways. Firstly the bottom dots in the grid are "eaten". Secondly at timed intervals a whole color disappears from across the board.
 
-To keep to game alive simply click the black spots to replace some of the colored spots at the top of the board as they disappear.
+To keep to game alive and achieve a higher score a player simply clicks the black spots. This adds a random number of new colored spots including one new black spot at the top of the board and counteracts the disappearing spots. The faster that you are able to click the black spots the more you offset the disappearance of spots.
 
-Your score is based on a combination of the number of spots you click and the number of black spots still on board at the time. This encourages you to try and save the lowest dots first in preference to just rapidly reclicking the same spot.
+Your score is based on a combination of the number of spots you click and the number of black spots still on board at the time. This encourages you to try and save the lowest dots (those most in danger of being "eaten") first in preference to just rapidly reclicking the same spot. The decision to randomly remove an entire color from the board was based on the discovery that in an earlier version of the game it was possible to hover over a blackspot and button mash. Similarly rewarding successful clicks with a random rather than fixed number of new spots avoided the newly generated black spot from always being added in the same point on the board which again would have allowed an indefinite amount of button mashing. While not completely offsetting this tactic these modifications do help to mitigate it's successfulness.
--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 $(function(){
   var $grid =$('.grid');
-  var $whatColor= { 1:"yellow", 2:"darkTeal", 3:"lightPink", 4:"purple", 5:"lightPurple", 6:"skyBlue", 7:"lightBabyBlue", 8:"rustyRed", 9:"brown",  10:"brightGreen", 11:"teal", 12:"bubblegum",  13:"blue", 14:"orangeBrown", 15:"black"};
-  var $redvsgreen= { 1:"darkgreen26", 2:"midgreen37", 3:"mildgreen47", 4: "lightgreen75", 5:"darkred31", 6:"midred40", 7:"darksalmon42", 8:"peachy68", 9:"black"};
+  var $whatColor= { 1: 'yellow', 2: 'darkTeal', 3: 'lightPink', 4: 'purple', 5: 'lightPurple', 6: 'skyBlue', 7: 'lightBabyBlue', 8: 'rustyRed', 9: 'brown',  10: 'brightGreen', 11: 'teal', 12: 'bubblegum',  13: 'blue', 14: 'orangeBrown', 15: 'black'};
+  var $redvsgreen= { 1: 'darkgreen26', 2: 'midgreen37', 3: 'mildgreen47', 4: 'lightgreen75', 5: 'darkred31', 6: 'midred40', 7: 'darksalmon42', 8: 'peachy68', 9: 'black'};
   // to be added at a later date
   // var $bluevsred={};
   var $black = $('.black');
-  var $create_grid =$(".create_grid");
-  var $medium =$("#medium");
-  var $restart = $("#restart");
-  var $redGreen=$("#redGreen");
+  var $create_grid =$('.create_grid');
+  var $medium =$('#medium');
+  var $restart = $('#restart');
+  var $redGreen=$('#redGreen');
   var $about = $('.about');
   var $score= $('.score');
   var $scoreboard = $('.scoreboard');
@@ -20,102 +20,113 @@ $(function(){
   var $updateScore = $('li').length;
   var $counter =0;
 
-  // about = "colors are disappearing. try to keep them alive. click black spots and each time you do new spots will be added at the top of the board."
-// https://en.wikipedia.org/wiki/Color_blindness#Diagnosis
-// https://en.wikipedia.org/wiki/Ishihara_test
+  // about = 'colors are disappearing. try to keep them alive. click black spots and each time you do new spots will be added at the top of the board.'
 
-$restart.hide();
-
-var gameOver =(function (){
-  $("body").css({"background-color": "red"});
-  $($grid).css({"background-color":"red"});
-  $($scoreboard).css({"font-size":"100px"});
-  $($grid).empty();
-  $medium.show();
   $restart.hide();
-  // $medium.
-});
 
-var startOver =(function (){
-  $playerscore = 0;
-  $scoreboard.html($playerscore);
-  $medium.show();
-  $restart.hide();
-  $($grid).empty();
-});
+  var gameOver =(function (){
+    $('body').css({'background-color':  'red'});
+    $($grid).css({'background-color': 'red'});
+    $($scoreboard).css({'font-size': '100px'});
+    $($grid).empty();
+    $medium.show();
+    $restart.hide();
+    // $medium.
+  });
 
-$restart.on('click', function (i){
-  startOver();
-  $("body").css({"background-color": "red"});
-  $($grid).css({"background-color":"red"});
-  $($scoreboard).css({"font-size":"100px"});
-  buildABoard();
-});
+  var startOver =(function (){
+    $playerscore = 0;
+    $scoreboard.html($playerscore);
+    $medium.show();
+    $restart.hide();
+    $($grid).empty();
+  });
+
+  $restart.on('click', function (i){
+    startOver();
+    $('body').css({'background-color': 'red'});
+    $($grid).css({'background-color': 'red'});
+    $($scoreboard).css({'font-size': '100px'});
+    buildABoard();
+  });
 
 
-$medium.on('click', function (i){
-  buildABoard();
-});
+  $medium.on('click', function (i){
+    buildABoard();
+  });
 
-var buildABoard =(function (){
-  $("body").css({"background-color": "white"});
-  $($grid).css({"background-color":"white"});
-  $($scoreboard).css({"font-size":"25px"});
-  $playerscore = 0;
-  $scoreboard.html($playerscore);
-  $medium.hide();
-  $restart.show();
-  $about.html("Quickly quickly. Keep the colors alive");
-  var $blackSquare= '<li class=black></li>';
-  $grid.prepend($blackSquare);
+  var buildABoard =(function (){
+    $('body').css({'background-color': 'white'});
+    $($grid).css({'background-color': 'white'});
+    $($scoreboard).css({'font-size': '25px'});
+    $playerscore = 0;
+    $scoreboard.html($playerscore);
+    $medium.hide();
+    $restart.show();
+    $about.html('Quickly quickly. Keep the colors alive');
+    var $blackSquare= '<li class=black></li>';
+    $grid.prepend($blackSquare);
     for (i=0; i<399; i++){
       var $newSquare= '<li class='+$whatColor[Math.ceil(Math.random()*15)]+'></li>';
       $grid.append($newSquare);
     }
-   var eatdot = setInterval(function(){
-     var dots = $(".black").length;
-       if (dots > 0){
-         $('li').last().remove();
-         console.log("eating");
+    var eatdot = setInterval(function(){
+      var dots = $('.black').length;
+      if (dots > 0){
+        $('li').last().remove();
+        console.log('eating');
 
-       }else {
-         gameOver();
-         clearInterval(eatdot);
-       }
-     },100);
+      }else {
+        gameOver();
+        clearInterval(eatdot);
+      }
+    },100);
     var dropclass = setInterval(function() {
-      var dots = $(".black").length;
-        if (dots > 0){
-          $colorToRemove = '.'+$whatColor[Math.ceil(Math.random()*14)];
-          console.log($colorToRemove);
-          $($colorToRemove).fadeOut();
-        }else{
-          clearInterval(dropclass);
-        }
+      var dots = $('.black').length;
+      if (dots > 0){
+        $colorToRemove = '.'+$whatColor[Math.ceil(Math.random()*14)];
+        console.log($colorToRemove);
+        $($colorToRemove).fadeOut();
+      }else{
+        clearInterval(dropclass);
+      }
 
-      },1500);
-});
+    },1500);
+  });
 // removes individual tiles that are black and adds a new black tile plus up to 5 random tiles at the start of the list
 
   $grid.on('click', '.black', function(i){
-      $livescore = $('.black').length;
-      $playerscore = $playerscore + $livescore + 100;
-      $scoreboard.html($playerscore);
-      $(this).remove(300 * i+100);
-      var $blackSquare= '<li class=black></li>';
-      $grid.prepend($blackSquare);
-      for (i=0; i<Math.ceil(Math.random()*5); i++){
-            var $newSquare= '<li class='+$whatColor[Math.ceil(Math.random()*15)]+'></li>';
-            $grid.prepend($newSquare);
-          }
-      });
+    $livescore = $('.black').length;
+    $playerscore = $playerscore + $livescore + 100;
+    $scoreboard.html($playerscore);
+    $(this).remove(300 * i+100);
+    var $blackSquare= '<li class=black></li>';
+    $grid.prepend($blackSquare);
+    for (i=0; i<Math.ceil(Math.random()*5); i++){
+      var $newSquare= '<li class='+$whatColor[Math.ceil(Math.random()*15)]+'></li>';
+      $grid.prepend($newSquare);
+    }
+  });
+});
 
+
+      // seeingred.setup = function(){
+      //   const seeingred = seeingred || {};
+      //
+      //   seeingred.setup(){
+      //     this.grid =$('.grid');
+      //     this.create_grid = $('.create_grid');
+      //     this.about = $('.about');
+      //   }
+      // };
+      //
+      // });
   //
   //     $create_grid.on('click', function (i){
-  //       $grid.css({"background-color": "white"});
-  //       $about.html("Quickly quickly.");
-  //       // $("#medium").removeClass("create_grid").addClass("reset");
-  //       // $(".reset").html("reset");
+  //       $grid.css({'background-color':  'white'});
+  //       $about.html('Quickly quickly.');
+  //       // $('#medium').removeClass('create_grid').addClass('reset');
+  //       // $('.reset').html('reset');
   //       var $blackSquare= '<li class=black></li>';
   //       $grid.prepend($blackSquare);
   //         for (i=0; i<399; i++){
@@ -124,10 +135,10 @@ var buildABoard =(function (){
   //           $grid.append($newSquare);
   //         }
   //        var eatdot = setInterval(function(){
-  //          var dots = $(".black").length;
+  //          var dots = $('.black').length;
   //            if (dots > 0){
   //              $('li').last().remove();
-  //              console.log("eating");
+  //              console.log('eating');
   //
   //            }else {
   //              gameOver();
@@ -135,7 +146,7 @@ var buildABoard =(function (){
   //            }
   //          },100);
   //         var dropclass = setInterval(function() {
-  //           var dots = $(".black").length;
+  //           var dots = $('.black').length;
   //             if (dots > 0){
   //               $colorToRemove = '.'+$redvsgreen[Math.ceil(Math.random()*8)];
   //               console.log($colorToRemove);
@@ -161,10 +172,10 @@ var buildABoard =(function (){
   //               }
   //           });
 
-  });
-  // var $small = $("#small");
-  // var $large =$("#large");
-  // var $play= $(".play");
+
+  // var $small = $('#small');
+  // var $large =$('#large');
+  // var $play= $('.play');
   // var $livescore = $('.black').length;
   // $grid.prepend($newSquare);
   // $score.html($('.black').length);
@@ -172,7 +183,7 @@ var buildABoard =(function (){
   // $score.html($('.black').length);
   // }
   // if ($livescore === 0) {
-  //           $body.css({'background-color': 'red'});
+  //           $body.css({'background-color':  'red'});
 
   // var $eatdot = setInterval(function(){
   //   $('li').last().remove();
@@ -192,10 +203,10 @@ var buildABoard =(function (){
 
   //   var $interval = setInterval(function() {
   //     $counter--;
-  //     $countdown.html($counter+" seconds");
+  //     $countdown.html($counter+' seconds');
   //     if ($counter === 0) {
   //         clearInterval($interval);
-  //         $body.css({'background-color': 'red'});
+  //         $body.css({'background-color':  'red'});
   //
   //     }
   // }, 1000);
@@ -208,29 +219,29 @@ var buildABoard =(function (){
   //     // var $counter = 30;
   //     var $interval = setInterval(function() {
   //       $counter--;
-  //       $countdown.html($counter+" seconds");
+  //       $countdown.html($counter+' seconds');
   //       if ($counter === 0) {
   //           clearInterval($interval);
-  //           $body.css({'background-color': 'red'});
-  //           $countdown.html("You lose!");
+  //           $body.css({'background-color':  'red'});
+  //           $countdown.html('You lose!');
   //           $grid.toggle();
   //           $('h2').fadeIn(300);
-  //           // $body.css({'background-color': 'white'}).fadeIn(10000);
+  //           // $body.css({'background-color':  'white'}).fadeIn(10000);
   //       }
   //     }, 1000);
   //
-  //   $about.html("Color blindness or Color Vision Deficit affects around 8% of men");
+  //   $about.html('Color blindness or Color Vision Deficit affects around 8% of men');
   //     for (i=0; i<100; i++){
   //       var $newSquare ='<li class='+$whatColor[Math.ceil(Math.random()*15)]+'></li>';
   //       $grid.append($newSquare);
-  //       $('li').css({'height': '50px', 'width': '50px'});
+  //       $('li').css({'height':  '50px', 'width':  '50px'});
   //       $('h2').fadeOut(300 * i+100);
   //       }
   //     });
 
   // $large.on('click', function (i){
-  //   $about.html("Colorblindness large");
-  //   console.log("clicked");
+  //   $about.html('Colorblindness large');
+  //   console.log('clicked');
   //     for (i=0; i<2500; i++){
   //       var $newSquare= '<li class='+$whatColor[Math.ceil(Math.random()*15)]+'></li>';
   //       $grid.append($newSquare);
@@ -248,20 +259,20 @@ var buildABoard =(function (){
 
   // var $interval = setInterval(function() {
   //     $counter--;
-  //     $countdown.html($counter+" seconds");
+  //     $countdown.html($counter+' seconds');
   //     // if ($counter === 0) {
   //     //     clearInterval($interval);
-  //     //     $body.css({'background-color': 'red'});
+  //     //     $body.css({'background-color':  'red'});
   //
   //     // }
   //   }, 1000);
 
   // $grid.on('click', '.darkTeal', function(i){
-  //     $(".darkTeal").fadeOut(300 * i+100);
+  //     $('.darkTeal').fadeOut(300 * i+100);
   // });
 
   // $grid.on('click', '.darkTeal', function(i){
-  //     $(".darkTeal").fadeOut(300 * i+100);
+  //     $('.darkTeal').fadeOut(300 * i+100);
   //     for (i=0; i< $('.darkTeal').length; i++){
   //       var $newSquare= '<li class='+$whatColor[Math.ceil(Math.random()*15)]+'></li>';
   //       $grid.append($newSquare);
@@ -270,44 +281,44 @@ var buildABoard =(function (){
   // });
 
   // $grid.on('click', '.lightPink', function(i){
-  //     $(".lightPink").fadeOut(300 * i+100);
+  //     $('.lightPink').fadeOut(300 * i+100);
   // });
   // $grid.on('click', '.bubblegum', function(i){
-  //     $(".bubblegum").fadeOut(300 * i+100);
+  //     $('.bubblegum').fadeOut(300 * i+100);
   // });
   // $grid.on('click', '.purple', function(i){
-  //     $(".purple").fadeOut(300 * i+100);
+  //     $('.purple').fadeOut(300 * i+100);
   // });
   // $grid.on('click', '.lightPurple', function(i){
-  //     $(".lightPurple").fadeOut(300 * i+100);
+  //     $('.lightPurple').fadeOut(300 * i+100);
   // });
   // $grid.on('click', '.skyBlue', function(i){
-  //     $(".skyBlue").fadeOut(300 * i+100);
+  //     $('.skyBlue').fadeOut(300 * i+100);
   // });
   // $grid.on('click', '.lightBabyBlue', function(i){
-  //     $(".lightBabyBlue").fadeOut(300 * i+100);
+  //     $('.lightBabyBlue').fadeOut(300 * i+100);
   // });
   // $grid.on('click', '.rustyRed', function(i){
-  //     $(".rustyRed").fadeOut(300 * i+100);
+  //     $('.rustyRed').fadeOut(300 * i+100);
   // });
   // $grid.on('click', '.brown', function(i){
-  //     $(".brown").fadeOut(300 * i+100);
+  //     $('.brown').fadeOut(300 * i+100);
   // });
   // $grid.on('click', '.brightGreen', function(i){
-  //     $(".brightGreen").fadeOut(300 * i+100);
+  //     $('.brightGreen').fadeOut(300 * i+100);
   // });
   // $grid.on('click', '.teal', function(i){
-  //     $(".teal").fadeOut(300 * i+100);
+  //     $('.teal').fadeOut(300 * i+100);
   // });
   // $grid.on('click', '.bubblegum', function(i){
-  //     $(".bubblegum").fadeOut(300 * i+100);
+  //     $('.bubblegum').fadeOut(300 * i+100);
   // });
   // $grid.on('click', '.blue', function(i){
-  //     $(".blue").fadeOut(300 * i+100);
+  //     $('.blue').fadeOut(300 * i+100);
   // });
   // $grid.on('click', '.orangeBrown', function(i){
-  //     $(".orangeBrown").fadeOut(300 * i+100);
+  //     $('.orangeBrown').fadeOut(300 * i+100);
   // });
   // $grid.on('click', '.yellow', function(i){
-  //     $(".yellow").fadeOut(300 * i+100);
+  //     $('.yellow').fadeOut(300 * i+100);
   // });
